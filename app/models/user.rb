f5db4e126640d8c4af433f541c2d7898ee8064de
youheishimada <dainoji11@gmail.com>
2025-05-20 18:24:04 +0900

    commit
    ## What
    - Userモデルに対して、以下のバリデーションを追加しました。
      - 氏名（漢字・ひらがな・カタカナ）の全角チェック
      - 氏名かな（ふりがな）は全角カタカナのみ許容
      - 各項目のpresence（必須）バリデーション
     - emailに関するバリデーション（presence, uniqueness, format）を削除
    - Userモデルに対し、英数字混在ルールを追加するパスワードのバリデーションを設定
    - ログアウトリンクから `turbo: false` を削除し、Rails 7における標準的なTurbo対応に変更
    - 実装済みの処理に残っていた実装案内コメントアウトを削除しました。
    ## Why
    - 入力項目ごとに適切な文字種の制限を設けることで、データの整合性と品質を確保するため。
    - 氏名かなにひらがなや漢字が入力された場合の不正登録を防止するため。
    - 日本語の姓名の入力ルールに沿った厳密なバリデーションを実現するため。
    - Deviseの`:validatable`モジュールによって自動的に適用されているため、重複定義を避けるため
    - 保守性・可読性を高め、今後のメンテナンスを簡潔にするため
    - Deviseのデフォルトでは文字種の制限がないため、英数字混在ルールを追加することで、より強固なパスワードを要求するため
    - 不要なオプションを削除し、コードを簡潔に保つため
    - コードの可読性とメンテナンス性を高めるため。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,13 @@ class User < ApplicationRecord
         
          #氏名/ニックネーム バリデーション設定 
          validates :nickname, presence: true
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください' }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: 'は全角で入力してください' }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は全角で入力してください' }
          validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
+         validates :first_name_kana, presence: true, format: { with:/\A[ァ-ヶー－]+\z/, message: 'は全角カタカナで入力してください' }
         
          #生年月日 バリデーション設定
          validates :birthday, presence: true
-        
-        #メールアドレス バリデーション設定
-         validates :email, presence: true, uniqueness: true
-         validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'には@を含めてください' }
         
          #パスワード バリデーション設定
          validates :password, format: { 
