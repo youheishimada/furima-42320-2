f01bb5d77249447a6d19cf1d2a53bddfe02d1a58
youheishimada <dainoji11@gmail.com>
2025-05-20 16:15:13 +0900

    Revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
  before_action :basic_auth
- before_action :configure_permitted_parameters, if: :devise_controller?
+#  before_action :configure_permitted_parameters, if: :devise_controller?
 
    def after_sign_out_path_for(resource_or_scope)
     root_path
@@ -14,10 +14,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
-   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [
-      :nickname, :last_name, :first_name,
-      :last_name_kana, :first_name_kana, :birthday
-    ])
-  end
+  #  def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [
+  #     :nickname, :last_name, :first_name,
+  #     :last_name_kana, :first_name_kana, :birthday
+  #   ])
+  # end
 end
