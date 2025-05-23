c1ef3f5a34ab9f4736d8e00cc2bf4aac67c52616
youheishimada <dainoji11@gmail.com>
2025-05-23 21:22:03 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :items, only: [:index, :new, :create] do
-  collection do
-    get :category_list
-    get :status_list
-  end
-end
+  resources :items, only: [:index, :new, :create] 
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
