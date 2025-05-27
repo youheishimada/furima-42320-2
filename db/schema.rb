f25bb6698d2bf2a858e3d6144626a8db9374d3ae
youheishimada <dainoji11@gmail.com>
2025-05-27 18:47:52 +0900

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_05_23_052319) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "items", force: :cascade do |t|
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "category_id"
@@ -29,7 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_23_052319) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
