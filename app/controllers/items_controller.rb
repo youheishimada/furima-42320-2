18f61ec1a8b640396ee5a927f3128027cc94907e
youheishimada <dainoji11@gmail.com>
2025-05-27 15:42:28 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,17 +22,17 @@ class ItemsController < ApplicationController
     end
  end
 
-  def edit
-  end
+  #def edit
+  #end
   
-  def update
-  end
+  #def update
+  #end
 
-  def destroy
-   @item = Item.find(params[:id])
-   @item.destroy
-   redirect_to root_path
-  end
+  #def destroy
+   #@item = Item.find(params[:id])
+   #@item.destroy
+   #redirect_to root_path
+  #end
 
   private
 
--- spec/factories/purchases.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :purchase do
-    user { nil }
-    item { nil }
-  end
-end
